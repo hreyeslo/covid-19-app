8416d28e5f5853a4c1999f3c0e707a5c2d72ebfc
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2020-03-25 22:44:26 +0100

    Add pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule, I18nModule } from '@app/core';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,7 +18,8 @@ import { AppComponent } from './app.component';
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		CoreModule,
-		I18nModule.forRoot('app')
+		I18nModule.forRoot('app'),
+		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
 	],
 	bootstrap: [AppComponent]
 })
