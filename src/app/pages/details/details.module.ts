b338f34a2ebdc4c86a8c55ad9b4f6a8f327df4c4
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2020-03-29 13:44:23 +0200

    Fix: pwa images

--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -2,6 +2,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { CountUpModule } from 'ngx-countup';
@@ -32,7 +33,8 @@ import { detailsReducer } from './store/details.reducer';
 		MatIconModule,
 		MatProgressSpinnerModule,
 		MatCardModule,
-		CountUpModule
+		CountUpModule,
+		MatTabsModule
 	]
 })
 export class DetailsModule {}
